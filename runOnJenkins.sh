64d06604d9b60a47698acf198c24e64c26fe658e
hexiuya <hexiuya0902@hotmail.com>
2018-09-26 11:25:40 +0800

    Jmeter test

--- runOnJenkins.sh
+++ runOnJenkins.sh
@@ -1,5 +1,15 @@
+###################
+# dockerising app #
+###################
 docker stop nginx_customised_c
 docker rm nginx_customised_c
 docker image rm nginx_customised_c
 docker build . -t nginx_customised_c
 docker run -d -p 80:80 --name nginx_customised_c --network crm-network --network-alias alias-nginx-c nginx_customised_c
+
+####################
+#Jmeter test starts#
+####################
+cd /home/xiuya/Apps/apache-jmeter-4.0/bin
+./jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t plans/crm-test2.jmx -l plans/crm-test2-testResult.jtl
+
